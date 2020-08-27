56a934cc0401445d06255943bbe5da39f4daaed3
Chen Gao <maxlin2905@gmail.com>
2020-08-27 22:27:11 +1000

    user bug fix

--- react_redux_demo/src/redux/user/userReducer.js
+++ react_redux_demo/src/redux/user/userReducer.js
@@ -1,31 +1,33 @@
-import{FETCH_USER_REQUEST,FETCH_USER_SUCESS,FETCH_USER_FAIL} from './userTypes'
+import { FETCH_USER_REQUEST, FETCH_USER_SUCESS, FETCH_USER_FAIL } from './userTypes'
 
-const initialState ={
-    loading:true,
-    users:[],
-    error:''
+const initialState = {
+    loading: true,
+    users: [],
+    error: ''
 }
 
-const useReducer =(state=initialState,action)=>{
-switch(action.type){
-    case FETCH_USER_REQUEST:
-        return {
-            ...state,
-            loading:true
-        }
-    case FETCH_USER_SUCESS:
-        return {
-        
-            loading:false,
-            users:action.payload,
-            user:''
-        }
+const useReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_USER_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case FETCH_USER_SUCESS:
+            return {
+
+                loading: false,
+                users: action.payload,
+                user: ''
+            }
         case FETCH_USER_FAIL:
             return {
                 ...state,
-                error:action.payload
+                error: action.payload
             }
-}
+        default:
+            return state
+    }
 
 }
 export default useReducer
